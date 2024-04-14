0ceaa1256cd489795bfbd03b5cceaf6e95763209
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2024-04-14 13:07:00 +0200

    Update hometask

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,14 +1,54 @@
-import React, { useEffect, useRef } from 'react';
+import React, { ReactElement, useEffect, useRef } from "react";
 
-// Опишіть Props
+// // Опишіть Props
+// export function Observer({ children, onContentEndVisible }: Props) {
+//   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
+  //const endContentRef = useRef(null);
+
+//   useEffect(() => {
+//     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
+//     const options = {
+//       rootMargin: '0px',
+//       threshold: 1.0,
+//       root: null,
+//     };
+
+//     const observer = new IntersectionObserver((entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.intersectionRatio > 0) {
+//           onContentEndVisible();
+//           observer.disconnect();
+//         }
+//       });
+//     }, options);
+
+//     if (endContentRef.current) {
+//       observer.observe(endContentRef.current);
+//     }
+
+//     return () => {
+//       observer.disconnect();
+//     };
+//   }, [onContentEndVisible]);
+
+//   return (
+//     <div>
+//       {children}
+//       <div ref={endContentRef} />
+//     </div>
+//   );
+// }
+interface Props {
+  children: ReactElement<HTMLDivElement>;
+  onContentEndVisible: () => void;
+}
+
 export function Observer({ children, onContentEndVisible }: Props) {
-  // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
+    const options: IntersectionObserverInit = {
+      rootMargin: "0px",
       threshold: 1.0,
       root: null,
     };
